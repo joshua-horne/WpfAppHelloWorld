c8c745b5adbc064145a2b3ddc1d43b84ae532004
Joshua-Horne <joshua.lee.horne@gmail.com>
2022-06-26 22:23:31 +1200

    Add project files.

---
+++ WpfAppHelloWorld/Greetings.xaml.vb
@@ -0,0 +1,9 @@
+﻿Class MainWindow
+    Private Sub DisplayButton_Click(sender As Object, e As RoutedEventArgs) Handles DisplayButton.Click
+        If HelloButton.IsChecked = True Then
+            MessageBox.Show("Hello.")
+        ElseIf GoodbyeButton.IsChecked = True Then
+            MessageBox.Show("Goodbye.")
+        End If
+    End Sub
+End Class
